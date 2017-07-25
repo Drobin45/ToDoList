d0c5d24253f181fd3b9e40f6c548dd8e4ff6b43f
drobin45 <drobin45@kent.edu>
2017-07-25 13:25:42 -0400

    Added Task class/controller and List class/controller

---
+++ ToDoList/Models/List.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class List
+    {
+        [Key]
+        public int ListID { get; set; }
+
+        [Display(Name = "List")]
+        public string ListTitle { get; set; }
+        public virtual ICollection<Task> Tasks { get; set; }
+
+    }
+}
